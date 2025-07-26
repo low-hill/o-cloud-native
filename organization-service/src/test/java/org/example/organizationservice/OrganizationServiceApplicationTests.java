8e4f1d1c7cb8cd60ce27fff2825c73f1de1568ad
김전영 <lowhill@techready.kr>
2025-07-26 22:40:35 +0900

    feat: eureka-server설정 - eureka-server설정 - organization서비스 init

---
+++ organization-service/src/test/java/org/example/organizationservice/OrganizationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.organizationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
